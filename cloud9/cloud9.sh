15c1ead890ec2230a53eaf17a7e974dcc17eca78
Sébastien FAUVART <sebf@ks312887.kimsufi.com>
2014-09-23 14:29:59 +0200

    update cloud9

--- cloud9/cloud9.sh
+++ cloud9/cloud9.sh
@@ -4,5 +4,7 @@
 NODE_ENV="production"
 export NODE_ENV
 #exec /sbin/setuser cloud9 /cloud9/bin/cloud9.sh -l 0.0.0.0 -w /workspace >> /var/log/cloud9.log 2>&1
-exec /bin/bash -l -c '/cloud9/bin/cloud9.sh $CLOUD9_PARAMS -l 0.0.0.0 -w /workspace' >> /var/log/cloud9.log 2>&1
+# fix bind port < 1024 for users
+exec setcap 'cap_net_bind_service=+ep' `which node`
+exec /bin/bash -l -c '/sbin/setuser www-data /cloud9/bin/cloud9.sh $CLOUD9_PARAMS -l 0.0.0.0 -w /workspace' >> /var/log/cloud9.log 2>&1
 
